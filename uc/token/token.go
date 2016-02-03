ce06ad7e14513b4fa3abe9e055254bc2055901b8
Alexander Andersson <sangisos@gmail.com>
2016-02-03 18:08:02 +0100

    Add tokens for μC

--- uc/token/token.go
+++ uc/token/token.go
@@ -1,1 +1,52 @@
 package token
+
+// Example taken from https://blog.gopheracademy.com/advent-2014/parsers-lexers/
+
+type Token int
+
+const (
+	// Special tokens
+	ILLEGAL Token = iota
+	EOF
+	WS          // blank (32), newline (10), carriage return (13), form feed (12), and tab (9)
+
+
+	IDENT       // identifiers (function names, variables, ...) [a-zA-Z_][a-zA-Z0-9_]*
+
+	// Literals
+	STRING
+	NUM
+
+	// Misc characters
+	NEQ         // !=
+	NOT         // !
+	AND         // &&
+	LPARAN      // (
+	RPARAN      // )
+	ASTERIX     // *
+	PLUS        // +
+	COMMA       // , (comma)
+	MINUS       // -
+	DIVIDE      // /
+	SEMICOLON   // ;
+	LE          // <=
+	LT          // <
+	EQ          // ==
+	ASSIGN      // =
+	GE          // >=
+	GT          // >
+	LBRACKET    // [
+	RBRACKET    // ]
+	LBRACE      // {
+	RBRACE      // }
+
+	// Keywords
+	CHAR
+	ELSE
+	IF
+	INT
+	RETURN
+	VOID        // function signatures return type only
+	WHILE
+
+)
