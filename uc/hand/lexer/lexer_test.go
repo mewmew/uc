46649e45e9f16d6e2a8b5513a9fd0594ada1f47c
Alexander Andersson <sangisos@gmail.com>
2016-02-21 01:46:02 +0100

    HANGS - test lexer: Add good lexer test

--- uc/hand/lexer/lexer_test.go
+++ uc/hand/lexer/lexer_test.go
@@ -81,8 +81,458 @@ func TestLexer(t *testing.T) {
 				},
 			},
 		},
+		{
+			path: "../../testdata/incorrect/lexer/good.c",
+			toks: []token.Token{
+				{
+					Kind: token.Comment,
+					Val:  "\t\t\t\t\t\t\t||\n**\tFile for testing lexical analysis\t\t||\n**\t\t\t\t\t\t\t||\n**\nThis file would confuse a parser, but\n        is 'lexically correct'.\t\t                ||\n*/\n\n/* ** / ** ",
+					Pos:  1,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " Simple tokens and single characters:",
+					Pos:  179,
+				},
+				{
+					Kind: token.Lbrace,
+					Val:  "{",
+					Pos:  220,
+				},
+				{
+					Kind: token.Rbrace,
+					Val:  "}",
+					Pos:  222,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " until end-of-line comment",
+					Pos:  248,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "if",
+					Pos:  277,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "else",
+					Pos:  280,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "while",
+					Pos:  285,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " normal comment ",
+					Pos:  311,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "return",
+					Pos:  332,
+				},
+				{
+					Kind: token.Land,
+					Val:  "&&",
+					Pos:  339,
+				},
+				{
+					Kind: token.Eq,
+					Val:  "==",
+					Pos:  342,
+				},
+				{
+					Kind: token.Ne,
+					Val:  "!=",
+					Pos:  345,
+				},
+				{
+					Kind: token.Le,
+					Val:  "<=",
+					Pos:  348,
+				},
+				{
+					Kind: token.Ge,
+					Val:  ">=",
+					Pos:  348,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "char",
+					Pos:  354,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "int",
+					Pos:  359,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "void",
+					Pos:  363,
+				},
+				{
+					Kind: token.Add,
+					Val:  "+",
+					Pos:  371,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  373,
+				},
+				{
+					Kind: token.Mul,
+					Val:  "*",
+					Pos:  375,
+				},
+				{
+					Kind: token.Div,
+					Val:  "/",
+					Pos:  377,
+				},
+				{
+					Kind: token.Lt,
+					Val:  "<",
+					Pos:  380,
+				},
+				{
+					Kind: token.Gt,
+					Val:  ">",
+					Pos:  382,
+				},
+				{
+					Kind: token.Assign,
+					Val:  "=",
+					Pos:  384,
+				},
+				{
+					Kind: token.Lparen,
+					Val:  "(",
+					Pos:  387,
+				},
+				{
+					Kind: token.Comma,
+					Val:  ",",
+					Pos:  388,
+				},
+				{
+					Kind: token.Semicolon,
+					Val:  ";",
+					Pos:  389,
+				},
+				{
+					Kind: token.Rparen,
+					Val:  ")",
+					Pos:  390,
+				},
+				{
+					Kind: token.Lbracket,
+					Val:  "[",
+					Pos:  392,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "was",
+					Pos:  393,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  396,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "colon",
+					Pos:  397,
+				},
+				{
+					Kind: token.Rbracket,
+					Val:  "]",
+					Pos:  402,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " Comment with bad tokens: _ || | ++ # @ ...  ",
+					Pos:  406,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " Ditto */ /* : _ || | ++ # @ ...  ",
+					Pos:  456,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " Identifiers and numbers:",
+					Pos:  493,
+				},
+				{
+					Kind: token.IntLit,
+					Val:  "17",
+					Pos:  522,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  525,
+				},
+				{
+					Kind: token.IntLit,
+					Val:  "17",
+					Pos:  526,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " No floats? -17.17e17 -17.17E-17  ",
+					Pos:  529,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "ponderosa",
+					Pos:  568,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "Black",
+					Pos:  578,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "Steel",
+					Pos:  584,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "PUMPKIN",
+					Pos:  590,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "AfterMath",
+					Pos:  598,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "aBBaoN",
+					Pos:  608,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "faT",
+					Pos:  615,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "TRacKs",
+					Pos:  619,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "K9",
+					Pos:  628,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "K23",
+					Pos:  631,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "B52",
+					Pos:  635,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "Track15",
+					Pos:  639,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "not4money",
+					Pos:  647,
+				},
+				{
+					Kind: token.IntLit,
+					Val:  "378",
+					Pos:  657,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "WHOIS666999SIOHM",
+					Pos:  661,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "was",
+					Pos:  687,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  690,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "floating",
+					Pos:  691,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  699,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "point",
+					Pos:  700,
+				},
+				{
+					Kind: token.Sub,
+					Val:  "-",
+					Pos:  705,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "number",
+					Pos:  706,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " The following 'trap' should be correctly handled:\n\n\t\t* \"2die4U\" consists of the number '2' and the\n\t\t  identifier 'die4U'.\n",
+					Pos:  714,
+				},
+				{
+					Kind: token.IntLit,
+					Val:  "2",
+					Pos:  852,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "die4U",
+					Pos:  853,
+				},
+				{
+					Kind: token.Comment,
+					Val:  "|| The following should all be regarded as identifiers:",
+					Pos:  860,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "Function",
+					Pos:  920,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "PrOceDuRE",
+					Pos:  929,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "begIN",
+					Pos:  939,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "eNd",
+					Pos:  945,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "PrinT",
+					Pos:  949,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "rEad",
+					Pos:  955,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "iF",
+					Pos:  960,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "THen",
+					Pos:  963,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "StaTic",
+					Pos:  968,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "ElSe",
+					Pos:  976,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "wHilE",
+					Pos:  981,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "Do",
+					Pos:  987,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "reTurN",
+					Pos:  990,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "noT",
+					Pos:  997,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "AnD",
+					Pos:  1001,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "OR",
+					Pos:  1005,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "TrUE",
+					Pos:  1008,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "bOOl",
+					Pos:  1013,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "FalsE",
+					Pos:  1018,
+				},
+				{
+					Kind: token.Ident,
+					Val:  "sizE",
+					Pos:  1024,
+				},
+				{
+					Kind: token.Comment,
+					Val:  " It is legal to end the code like this, without an ending newline.",
+					Pos:  1031,
+				},
+				{
+					Kind: token.EOF,
+					Val:  "",
+					// TODO: Figure out how to handle the offset in error cases.
+					Pos: 1100,
+				},
+			},
+		},
 		// TODO: Add tokens for the following test cases.
-		{path: "../../testdata/incorrect/lexer/good.c"},
 		{path: "../../testdata/incorrect/lexer/long-char.c"},
 		{path: "../../testdata/incorrect/lexer/ugly.c"},
 		{path: "../../testdata/quiet/lexer/l01.c"},
@@ -93,7 +543,7 @@ func TestLexer(t *testing.T) {
 		{path: "../../testdata/quiet/lexer/l06.c"},
 	}
 
-	for _, g := range golden {
+	for looprun, g := range golden {
 		log.Println("path:", g.path)
 		tokens, err := lexer.ParseFile(g.path)
 		if err != nil {
@@ -120,7 +570,9 @@ func TestLexer(t *testing.T) {
 				break
 			}
 		}
-		break // TODO: Remove this break to test all test cases and not just the first.
+		if looprun >= 1 {
+			break // TODO: Remove this break to test all test cases and not just the first.
+		}
 	}
 }
 
