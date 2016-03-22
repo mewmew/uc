508f40d3b8cbe561838ec1209f4d40766bce77eb
mewmew <rnd0x00@gmail.com>
2016-03-22 17:57:24 +0100

    cmd/ulex: Implement support for lexing from standard input.

--- cmd/ulex/ulex.go
+++ cmd/ulex/ulex.go
@@ -1,3 +1,7 @@
+// ulex is a lexer for the µC language which pretty-prints tokens to standard
+// output.
+//
+// Usage: ulex FILE...
 package main
 
 import (
@@ -5,31 +9,51 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"os"
 
 	"github.com/mewmew/uc/uc/hand/lexer"
+	"github.com/mewmew/uc/uc/hand/token"
 )
 
 func main() {
 	flag.Parse()
-	for _, path := range flag.Args() {
-		fmt.Printf("Lexing %q\n", path)
-		fmt.Println()
+	paths := flag.Args()
+	if flag.NArg() == 0 {
+		paths = append(paths, "-")
+	}
+	for _, path := range paths {
 		err := lexFile(path)
 		if err != nil {
 			log.Print(err)
 		}
-		fmt.Println()
 	}
 }
 
-func lexFile(path string) error {
-	toks, err := lexer.ParseFile(path)
-	if err != nil {
-		return err
+// lexFile lexes the given file and pretty-prints its tokens to standard output.
+func lexFile(path string) (err error) {
+	var toks []token.Token
+	if path == "-" {
+		fmt.Println("Lexing from standard input")
+		toks, err = lexer.Parse(os.Stdin)
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Printf("Lexing %q\n", path)
+		toks, err = lexer.ParseFile(path)
+		if err != nil {
+			return err
+		}
 	}
+	fmt.Println()
 	pad := int(math.Ceil(math.Log10(float64(len(toks)))))
 	for i, tok := range toks {
-		fmt.Printf("token %*d:   %v\n", pad, i, tok)
+		if tok.Kind == token.Error {
+			fmt.Printf("ERROR %*d:   %v\n", pad, i, tok)
+		} else {
+			fmt.Printf("token %*d:   %v\n", pad, i, tok)
+		}
 	}
+	fmt.Println()
 	return nil
 }
