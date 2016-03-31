9cae1a7f40de433a4c95194097f55b372e91c669
mewmew <rnd0x00@gmail.com>
2016-03-31 02:26:34 +0200

    grammar: Merge TypeName, VoidType and FuncType into TypeName. Fixes #26.

--- types/types.go
+++ types/types.go
@@ -1,1 +1,27 @@
 package types
+
+// TODO: Implement type checking which ensures correct uses of "void".
+// Relevant sections of the uC BNF grammar have been included below.
+//
+//    TopLevelDecl
+//       : VarDecl ";"
+//       | TypeName ident "(" Formals ")" FuncBody // TypeName : "char" | "int" | "void" ;
+//    ;
+//
+//    ScalarDecl
+//       : TypeName ident // TypeName : "char" | "int" ;
+//    ;
+//
+//    ArrayDecl
+//       : TypeName ident "[" int_lit "]" // TypeName : "char" | "int" ;
+//    ;
+//
+//    Formals
+//       : TypeName   // TypeName : "void" ;
+//       | FormalList
+//    ;
+//
+//    FormalDecl
+//       : ScalarDecl
+//       | TypeName ident "[" "]" // TypeName : "char" | "int" ;
+//    ;
