32f2f78a2751d68496027682109cae289c3652d7
Alexander Andersson <sangisos@gmail.com>
2016-05-01 15:08:35 +0200

    testcases: Fix lexically invalid semantic test case

--- testdata/extra/semantic/missing-return.c
+++ testdata/extra/semantic/missing-return.c
@@ -3,22 +3,20 @@
 //    warning: control reaches end of non-void function
 //
 // Undefined behaviour
-//    clang returns 106
-//    gcc return 45
+//    clang returns 13
+//    gcc return 17
 int f(int a) {
-	int d =111;
-	a;
-	//d;
+	;
 }
 
-int d(int a) {
+int g(int a) {
 	return a;
 }
 
 int main(void){
-	int a =3434;
-	int b =45;
-	int c =24;
-	a=d(a);
-	return f(b);
+	int a;
+	a=13;
+	g(a);
+	a=17;
+	return f(a);
 }
