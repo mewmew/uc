47aeebd0c36619d43de28d55c962774fa3a8ee04
Alexander Andersson <sangisos@gmail.com>
2016-05-04 02:16:06 +0200

    sem/semcheck: Add nested function check function

---
+++ testdata/extra/semantic/nested-function-def.c
@@ -0,0 +1,9 @@
+// Missing return is valid for main.
+//
+// "reaching the `}` that terminates the main // function returns a value of 0"
+// (§5.1.2.2.3 in the C11 spec)
+int main(void) {
+	void f(void){
+		;
+	}
+}
