02f1dd3b015d0e83ced251db0a366fdf8d46f7b4
Alexander Andersson <sangisos@gmail.com>
2016-05-22 22:49:02 +0200

    irgen: Mid refactoring of resolve with generator

--- irgen/irgen.go
+++ irgen/irgen.go
@@ -17,34 +17,44 @@ import (
 	uctypes "github.com/mewmew/uc/types"
 )
 
+type generator struct {
+	// module is the current module being created
+	module ir.Module
+	// funcDefStack holds all current function nests
+	funcDefStack []*ir.Function
+	// currentFunction points to the function being created
+	currentFunction *ir.Function
+	// basicBlocks holds basic blocks before function creation
+	basicBlocks []*ir.BasicBlock
+	// instructionBuffer is appens instruction before basic block creation.
+	instructionBuffer []instruction.Instruction
+	// ssaCounter give a unique id for anonymous assignments and basic blocks.
+	ssaCounter int
+	// lastLabel holds the ssa of the last created basic block
+	lastLabel int
+	// lastLabel holds the ssa of the last created basic block
+	recurse func(ast.Node) error
+}
+
 // Gen generates LLVM IR based on the syntax tree of the given file.
 func Gen(file *ast.File) error {
 	// TODO: REMOVE log messages
 	log.SetPrefix(term.BlueBold("Log:"))
 	log.SetFlags(log.Lshortfile)
+	var gen generator
+	// The usual size of a basic block seems to be less than 10 instructions.
+	gen.instructionBuffer = make([]instruction.Instruction, 0, 10)
 
-	var module ir.Module
-
-	var funcDefStack []*ir.Function
-	var currentFunction *ir.Function
-	var basicBlocks []*ir.BasicBlock
-	var instructionBuffer []instruction.Instruction
 	var insts []instruction.Instruction
 
-	// NOTE: Motivate initial capacity of 10.
-	instructionBuffer = make([]instruction.Instruction, 0, 10)
-
-	// ssaCounter is counted up for anonymous assignments and basic blocks to
-	// give them a unique id
-	ssaCounter := 0
-	lastLabel := ssaCounter
-	var recurse func(ast.Node) error
-	recurse = func(n ast.Node) error {
+	gen.ssaCounter = 0
+	gen.lastLabel = gen.ssaCounter
+	gen.recurse = func(n ast.Node) error {
 		switch n := n.(type) {
 		case *ast.File:
 			for _, decl := range n.Decls {
 
-				if err := recurse(decl); err != nil {
+				if err := gen.recurse(decl); err != nil {
 					return errutil.Err(err)
 				}
 			}
@@ -52,7 +62,7 @@ func Gen(file *ast.File) error {
 			for _, blockItem := range n.Items {
 				log.Printf("blockItem %#v of type %T\n", blockItem, blockItem)
 
-				if err := recurse(blockItem); err != nil {
+				if err := gen.recurse(blockItem); err != nil {
 					return errutil.Err(err)
 				}
 			}
@@ -64,7 +74,7 @@ func Gen(file *ast.File) error {
 			// really be done in parser... the following currently breakes
 			// ability to call nested functions
 			var name string
-			for _, prevfn := range funcDefStack {
+			for _, prevfn := range gen.funcDefStack {
 				name += prevfn.Name() + "."
 			}
 			name += n.Name().String()
@@ -75,7 +85,7 @@ func Gen(file *ast.File) error {
 			} else {
 				panic(errutil.Newf("Conversion from uc function type failed: %#v %#v", sig, n.Type()))
 			}
-			module.Funcs = append(module.Funcs, fn)
+			gen.module.Funcs = append(gen.module.Funcs, fn)
 
 			if !astutil.IsDef(n) {
 				log.Printf("create function decl %v\n", fn)
@@ -83,42 +93,42 @@ func Gen(file *ast.File) error {
 			}
 
 			log.Printf("create function def %v\n", fn)
-			funcDefStack = append(funcDefStack, fn)
-			currentFunction = fn
+			gen.funcDefStack = append(gen.funcDefStack, fn)
+			gen.currentFunction = fn
 
 			// Recurse body
-			if err := recurse(n.Body); err != nil {
+			if err := gen.recurse(n.Body); err != nil {
 				return errutil.Err(err)
 			}
 			var terminal instruction.Terminator
-			terminal, ssaCounter = endFunction(fn, ssaCounter)
-			allInsts := make([]instruction.Instruction, len(instructionBuffer))
-			copy(allInsts, instructionBuffer)
-			instructionBuffer = instructionBuffer[:0]
-			bb, err := ir.NewBasicBlock(toLocalVarString(lastLabel), allInsts, terminal)
+			terminal, gen.ssaCounter = endFunction(fn, gen.ssaCounter)
+			allInsts := make([]instruction.Instruction, len(gen.instructionBuffer))
+			copy(allInsts, gen.instructionBuffer)
+			gen.instructionBuffer = gen.instructionBuffer[:0]
+			bb, err := ir.NewBasicBlock(toLocalVarString(gen.lastLabel), allInsts, terminal)
 			if err != nil {
 				return errutil.Err(err)
 			}
-			basicBlocks = append(basicBlocks, bb)
-			log.Print(basicBlocks)
-			ssaCounter++
-			funcDefStack = funcDefStack[:len(funcDefStack)-1]
-			if len(funcDefStack) > 0 {
-				currentFunction = funcDefStack[len(funcDefStack)-1]
+			gen.basicBlocks = append(gen.basicBlocks, bb)
+			log.Print(gen.basicBlocks)
+			gen.ssaCounter++
+			gen.funcDefStack = gen.funcDefStack[:len(gen.funcDefStack)-1]
+			if len(gen.funcDefStack) > 0 {
+				gen.currentFunction = gen.funcDefStack[len(gen.funcDefStack)-1]
 			} else {
-				currentFunction = nil
+				gen.currentFunction = nil
 			}
 			return nil
 		case *ast.CallExpr:
-			insts, ssaCounter = createCall(n, ssaCounter)
-			instructionBuffer = append(instructionBuffer, insts...)
+			insts, gen.ssaCounter = createCall(n, gen.ssaCounter)
+			gen.instructionBuffer = append(gen.instructionBuffer, insts...)
 		case *ast.VarDecl:
 			if uctypes.IsVoid(n.Type()) {
 				return nil
 			}
-			if len(funcDefStack) > 0 {
-				insts, ssaCounter = createLocal(n, ssaCounter)
-				instructionBuffer = append(instructionBuffer, insts...)
+			if len(gen.funcDefStack) > 0 {
+				insts, gen.ssaCounter = createLocal(n, gen.ssaCounter)
+				gen.instructionBuffer = append(gen.instructionBuffer, insts...)
 			} else {
 				// Global values are compile time constant, no need for ssa
 				// NOTE: Global variables may still be unnamed, but they would have
@@ -128,11 +138,11 @@ func Gen(file *ast.File) error {
 				//    localCounter := 0
 				//    globalCounter := 0
 				gv := createGlobal(n)
-				module.Globals = append(module.Globals, gv)
+				gen.module.Globals = append(gen.module.Globals, gv)
 			}
 		case *ast.Ident:
-			ssaCounter++
-			log.Printf("loading ident %v into %v", n.Name, toLocalVarString(ssaCounter))
+			gen.ssaCounter++
+			log.Printf("loading ident %v into %v", n.Name, toLocalVarString(gen.ssaCounter))
 			typ := toIrType(n.Decl.Type())
 			var newVar value.Value
 			var err error
@@ -160,14 +170,14 @@ func Gen(file *ast.File) error {
 			if err != nil {
 				return errutil.Err(err)
 			}
-			instructionBuffer = append(instructionBuffer, instr)
+			gen.instructionBuffer = append(gen.instructionBuffer, instr)
 		case *ast.BinaryExpr:
 			log.Println("bin expr")
 			log.Println(n)
-			if err := recurse(n.Y); err != nil {
+			if err := gen.recurse(n.Y); err != nil {
 				return errutil.Err(err)
 			}
-			_ = ssaCounter
+			_ = gen.ssaCounter
 			switch n.Op {
 			case token.Assign:
 				log.Println(n.X)
@@ -177,60 +187,60 @@ func Gen(file *ast.File) error {
 				}
 				//default:
 			}
-			ssaCounter++
-			log.Printf("Assign %v %v %v to %#v", n.X, n.Op, n.Y, toLocalVarString(ssaCounter))
+			gen.ssaCounter++
+			log.Printf("Assign %v %v %v to %#v", n.X, n.Op, n.Y, toLocalVarString(gen.ssaCounter))
 
 		case *ast.ExprStmt:
-			if err := recurse(n.X); err != nil {
+			if err := gen.recurse(n.X); err != nil {
 				return errutil.Err(err)
 			}
 		case *ast.WhileStmt:
 			// Finnish last basic block with a branch to the basic block we
 			// are about to create (with label ssaCounter+1)
-			ssaCounter++
-			whileLabel := ssaCounter
-			allInsts := make([]instruction.Instruction, len(instructionBuffer))
-			copy(allInsts, instructionBuffer)
+			gen.ssaCounter++
+			whileLabel := gen.ssaCounter
+			allInsts := make([]instruction.Instruction, len(gen.instructionBuffer))
+			copy(allInsts, gen.instructionBuffer)
 			log.Printf("Clear last basic block instrucitons: %v\n", allInsts)
 			brToWhileLabel, err := instruction.NewBr(nil, toLocalVarString(whileLabel), "")
 			if err != nil {
 				return errutil.Err(err)
 			}
-			//terminator, ssaCounter = createWhile(n, ssaCounter)
-			bb, err := ir.NewBasicBlock(toLocalVarString(lastLabel), allInsts, brToWhileLabel)
+			//terminator, gen.ssaCounter = createWhile(n, gen.ssaCounter)
+			bb, err := ir.NewBasicBlock(toLocalVarString(gen.lastLabel), allInsts, brToWhileLabel)
 			if err != nil {
 				return errutil.Err(err)
 			}
-			basicBlocks = append(basicBlocks, bb)
+			gen.basicBlocks = append(gen.basicBlocks, bb)
 
-			ssaCounter++
-			lastLabel = ssaCounter
-			instructionBuffer = instructionBuffer[:0]
+			gen.ssaCounter++
+			gen.lastLabel = gen.ssaCounter
+			gen.instructionBuffer = gen.instructionBuffer[:0]
 
 			// Recurse over body of while loop
 			log.Printf("while.Body = %#v\n", n.Body)
 
-			if err := recurse(n.Body); err != nil {
+			if err := gen.recurse(n.Body); err != nil {
 				return errutil.Err(err)
 			}
 
-			//_, ssaCounter = endWhile(n, ssaCounter)
+			//_, gen.ssaCounter = endWhile(n, gen.ssaCounter)
 
 			// End the while loop with a branch to whileLabel
-			bb, err = ir.NewBasicBlock(toLocalVarString(lastLabel), allInsts, brToWhileLabel)
+			bb, err = ir.NewBasicBlock(toLocalVarString(gen.lastLabel), allInsts, brToWhileLabel)
 			if err != nil {
 				return errutil.Err(err)
 			}
-			basicBlocks = append(basicBlocks, bb)
-			ssaCounter++
-			lastLabel = ssaCounter
-			instructionBuffer = instructionBuffer[:0]
+			gen.basicBlocks = append(gen.basicBlocks, bb)
+			gen.ssaCounter++
+			gen.lastLabel = gen.ssaCounter
+			gen.instructionBuffer = gen.instructionBuffer[:0]
 		}
 		// TODO: Implement the rest of the needed node types
 		return nil
 	}
 
-	recurse(file)
+	gen.recurse(file)
 	return nil
 }
 
