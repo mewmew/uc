b2ddd2b0e22e0343f281cd00fb9407e8f117784c
mewmew <rnd0x00@gmail.com>
2016-06-19 00:08:57 +0200

    testdata: Add missing return statement to getstring of the uc.c runtime library.

--- testdata/uc.c
+++ testdata/uc.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 void putint(int x) {
-   printf("%d",x);
+	printf("%d", x);
 }
 
 void putstring(char s[]) {
-  fputs(s,stdout);
+	fputs(s, stdout);
 }
 
 int getint(void){
-  int i;
-  scanf("%d", &i);
-  return i;
+	int i;
+	scanf("%d", &i);
+	return i;
 }
 
 
 int getstring(char s[]) {
-  scanf("%s",s);
+	return scanf("%s", s);
 }
-
